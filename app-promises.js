dd4e2bb1a9dde2540e8c6ed195d4a59f71031d52
edo9k <edox86@gmail.com>
2018-08-23 17:59:24 -0300

    Added first async functions
    There are still some empty files that will be used in the following
    lessons.

--- app-promises.js
+++ app-promises.js
@@ -56,8 +56,25 @@ const getStatus = userId => {
   })
 }
 
-getStatus(2).then(status => {
-  console.log(status)
-}).catch(e => {
-  console.log(e)
-})
+const getStatusAlt = async userId => {
+  const user = await getUser(userId)
+  const grades = await getGrades(user.schoolId)
+
+  let average = 0
+
+  if (grades.length > 0) {
+    average = grades
+                .map(grade => grade.grade)
+                .reduce((a, b) => a + b) / grades.length
+  }
+
+  return `${user.name} has a ${average}% in the class.`
+}
+
+
+//getStatus(2).then(status => {
+//  console.log(status)
+//}).catch(e => {
+//  console.log(e)
+//})
+
